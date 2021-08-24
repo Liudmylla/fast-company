1fa7204ae320e884fe02518ceea8a45981fe951c
Myla <liudmyla12@yahoo.fr>
2021-08-24 16:16:52 +0200

    Users table

---
+++ src/components/users.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import api from '../api'
+import 'bootstrap/dist/css/bootstrap.css'
+const Users = () => {
+    const [users, setUsers] = useState(api.users.fetchAll())
+    const handleDelete = (userId) => {
+
+        setUsers(users.filter(user => user._id !== userId))
+    }
+    const renderFrase = (number) => {
+        let result
+
+        switch (number) {
+
+            case 0:
+                result = "Пичалька, никто не тусанет с тобой сегодня"
+                break;
+
+            case 2:
+            case 3:
+            case 4:
+
+                result = number + "  человекa тусанет с тобой сегодня"
+
+                break;
+            default:
+                result = number + "  человек тусанет с тобой сегодня"
+                break;
+        }
+        return result
+
+    }
+    const getColor = (color) => {
+        return "badge m-2 bg-" + color
+    }
+    const chooseColor = () => {
+        return users.length !== 0 ? 'primary' : 'danger'
+    }
+    return (
+        <>
+            <div className={getColor(chooseColor())}>{renderFrase(users.length)}</div>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th scope="col">Имя</th>
+                        <th scope="col">Качества</th>
+                        <th scope="col">Профессия</th>
+                        <th scope="col">Встретился,раз</th>
+                        <th scope="col">Оценка</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user) => (
+
+                        <tr key={user._id}>
+                            <td>{user.name} </td>
+                            <td>
+                                {user.qualities.map((q) => (
+                                    <span
+                                        className={getColor(q.color)}
+                                    >{q.name}</span>
+                                ))}
+                            </td>
+                            <td>
+                                {user.profession.name}
+                            </td>
+                            <td>{user.completedMeetings}</td>
+                            <td>{user.rate} </td>
+                            <td><button
+                                className="btn btn-danger"
+                                onClick={() => handleDelete(user._id)}
+                            >delete</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
+};
+
+
+export default Users;
